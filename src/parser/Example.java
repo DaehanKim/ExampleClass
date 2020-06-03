42443eea3929f9de04f29a37c68a43090792c291
DaehanKim <kdh5852@gmail.com>
2020-06-03 15:12:55 +0900

    choose implemented

--- src/parser/Example.java
+++ src/parser/Example.java
@@ -1,5 +1,6 @@
 package parser;
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Random;
 
@@ -28,25 +29,21 @@ public class Example {
 		return outStr;
 	}
 	
-	public String [] interpretKeyword(String keyword) {
+	public ArrayList<String> interpretKeyword(String keyword) {
 //	returns output numbers or words	
 		keyword = keyword.replace("\"", "");
 		String[] kSplit = keyword.split("\\\\t",-1);
-//		System.out.println(kSplit[0]);
-//		for(String item: kSplit) {
-//			System.out.println(item);
-//		}
 		switch (kSplit[0]) {
 		case "CHOOSE":
 			return this.choose(kSplit);
 		case "NUM":
-			return this.choose(kSplit);
+			return this.num(kSplit);
 		case "SAMPLE":
-			return this.choose(kSplit);
+			return this.sample(kSplit);
 		case "EVAL":
-			return this.choose(kSplit);
+			return this.eval(kSplit);
 		case "IMAGE":
-			return this.choose(kSplit);
+			return this.image(kSplit);
 		}
 		
 		return null;
@@ -55,27 +52,48 @@ public class Example {
 	
 	
 	
-	private String [] choose(String[] kSplit) {
+
+
+	private ArrayList<String> eval(String[] kSplit) {
 		// TODO Auto-generated method stub
-		int n = Integer.parseInt(kSplit[1]);
-		String[] optList = kSplit[2].split(",");
-		boolean flag = true;
-		while(flag) {
-		int rnd = new Random().nextInt(optList.length);
-		String output = optList[rnd];
-		if (kSplit[2] != null) {
-//			update flag
-			flag = this.checkCondition(output, kSplit[3]);
-		}
-		}
 		return null;
 	}
 
-	private boolean checkCondition(String output, String cond) {
+	private ArrayList<String> image(String[] kSplit) {
 		// TODO Auto-generated method stub
-		if (cond.length() == 0) {
-			return false;
+		return null;
+	}
+
+	private ArrayList<String> sample(String[] kSplit) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private ArrayList<String> num(String[] kSplit) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private ArrayList<String> choose(String[] kSplit) {
+		// implement choose method
+		int n = Integer.parseInt(kSplit[1]);
+		String[] optList = kSplit[2].split(",");
+		ArrayList<String> outputList = new ArrayList<String>();
+		ArrayList<Integer> indexList = new ArrayList<Integer>();
+		
+		// sample options from candidates without replacement
+		while (outputList.size() < n) {
+			int rnd = new Random().nextInt(optList.length);
+			if (indexList.contains(rnd)) { continue; }
+			outputList.add(optList[rnd]);
+			indexList.add(rnd);
 		}
+				
+		return outputList;
+	}
+
+	private boolean checkCondition(String output, String cond) {
+		// not implemented yet
 		return false;
 	}
 
@@ -84,7 +102,7 @@ public class Example {
 		String exTemplate = "$y=[a]x+[b]$가 x축과 만나는 점 --> ([x1],0)\r\n" + 
 				"y축과 만나는 점 --> (0,[y1])";
 		String exDescription = "x에 0을 넣으면 y절편, y에 0을 넣으면 x절편을 구할 수 있습니다. 여기에서 x절편은 ([x1],0), y절편은 (0,[y1])가 되겠네요!";
-		String exConditionJson = "{\"[a]\":\"CHOOSE\\t-1,1,-2,2\\t\",\r\n" + 
+		String exConditionJson = "{\"[a]\":\"CHOOSE\\t2\\tthis,is,-2,2,3,5\\t\",\r\n" + 
 				"\"[b]\":\"NUM\\tint\\t-2,3\\t\",\r\n" + 
 				"\"[x1]\":\"EVAL\\t-[b]/[a]\",\r\n" + 
 				"\"[y1]\":\"EVAL\\t[b]\"\r\n" + 
@@ -92,26 +110,16 @@ public class Example {
 		
 		
 		Example parser = new Example();
-		
-//		System.out.println(exTemplate);
-//		System.out.println(exDescription);
+
 		
 		JsonElement condition = parser.parseCondition(exConditionJson);
-//		System.out.println(condition.getAsJsonObject().entrySet());
-//		System.out.println(condition.getAsJsonObject().entrySet());
+
 		for(Map.Entry<String, JsonElement> entry : condition.getAsJsonObject().entrySet()) {
 					System.out.println(entry.getKey() + " - " + entry.getValue().toString());
-					parser.interpretKeyword(entry.getValue().toString());
+					ArrayList<String> out = parser.interpretKeyword(entry.getValue().toString());
+					System.out.println(out);
 				}
-//		KeywordExecutor executor = new KeywordExecutor();
-//		executor.execute(condition);
-//		JsonElement conditionOutput = executor.execute(condition);
-//		String repTemplate = parser.replaceWithJsonElement(exTemplate, conditionOutput);
-//		String repDescription = parser.replaceWithJsonElement(exDescription, conditionOutput);
-		
 
-//		System.out.println(repTemplate);
-//		System.out.println(repDescription);
 		
 		
 	}
